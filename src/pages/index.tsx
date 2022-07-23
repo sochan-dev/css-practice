08e4d99f6092216eb9cc0c3bffe85abdede0d803
sotatsu kitajima <halosakakitajima@gmail.com>
2022-07-23 16:55:06 +0900

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Styles from '/styles/Home.module.scss';
+import { PageLinkList } from 'home/components/organisms/PageLinkList';
+import Styles from 'home/styles/home.module.scss';
 
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>各都道府県の人工推移をグラフで比較してみよう</title>
-      <meta name="description" content="HTML＋CSS コーディングの強化書 EXERCISE 1" />
+      <title>HTML＋CSS コーディングの強化書</title>
+      <meta name="description" content="top page" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <div>
-      <div>
-        <p>
-          <a href="./exercise/1">EXERCISE 1</a>
-        </p>
-      </div>
+    <div className={Styles.container}>
+      <PageLinkList />
     </div>
   </>
 );
